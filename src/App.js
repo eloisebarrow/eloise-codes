9316aace01bc4efa6758e7ec0fb6a53dbab4e74e
eloisebarrow <eloisebarrow@gmail.com>
2019-11-15 09:52:27 -0500

    form mostly complete, no handleSubmit functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import HorizontalScroll from './components/HorizontalScroll';
 import ProgressBar from './components/ProgressBar';
@@ -6,14 +6,43 @@ import Effect from './components/Effect'; // useEffect counter example
 import ReactPlayerComp from './components/ReactPlayerComp'; // embed a video
 import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <HorizontalScroll />
-      <ProgressBar /> */}
-      <Form />
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: ''
+      }
+    }
+  }
 
-export default App;
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState( prevState => ({
+      formData: {
+        ...prevState.formData,
+        [name]: value
+      }
+    }))
+  }
+
+  handleSubmit = async () => {
+    // post method
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <HorizontalScroll />
+        <ProgressBar /> */}
+        <Form
+          formData={this.state.formData}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,46 +1,48 @@
 import React from 'react';
 import './Form.css';
 
-export default function Form() {
+export default function Form(props) {
   return (
     <div className="form-container">
       <form
         onSubmit={(e) => {
           e.preventDefault()
-          // props.handleSubmit()
+          props.handleSubmit()
         }}
       >
         <label htmlFor="first_name">First Name: </label>
         <input
-          // value={}
+          value={props.formData.first_name}
           type="text"
           name="first_name"
-          // onChange={(e) => props.handleFormChange(e)} 
+          onChange={(e) => props.handleChange(e)} 
           />
 
         <label>Last Name: </label>
         <input
-          // value={}
+          value={props.formData.last_name}
           type="text"
           name="last_name"
-          // onChange={(e) => props.handleFormChange(e)} 
+          onChange={(e) => props.handleChange(e)} 
           />
 
         <label>Email: </label>
         <input
-          // value={}
+          value={props.formData.email}
           type="email"
           name="email"
-          // onChange={(e) => props.handleFormChange(e)} 
+          onChange={(e) => props.handleChange(e)} 
           />
 
         <label>Password: </label>
         <input
-          // value={}
+          value={props.formData.password}
           type="password"
           name="password"
-          // onChange={(e) => props.handleFormChange(e)} 
+          onChange={(e) => props.handleChange(e)} 
           />
+
+          <button>Submit</button>
 
       </form>
     </div>
